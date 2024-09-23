f97b46c7861f20839ed1b47fb0a4edf8a7255ffc
UrakadakiRemuru <met-zi@mail.ru>
2024-09-23 13:40:23 +0300

    4_safe_text done

--- 1_strings/4_safe_text/main.py
+++ 1_strings/4_safe_text/main.py
@@ -20,6 +20,34 @@ def get_wrong_article() -> str:
 
 def recover_article() -> str:
     wrong_article = get_wrong_article()
+    list_of_raws = wrong_article.split('\n')
+    sentence = ''
+    pattern = 'woof-woof'
+    sign = '!'
+    for raw in list_of_raws:
+        exclamation_mark_counter = 1
+        result_list = []
+        if not len(raw): continue
+        for word in raw.split():
+            exclamation_mark_pointer = word.find(sign)
+            if exclamation_mark_pointer != -1:
+                exclamation_mark_counter += len(word[:exclamation_mark_pointer])
+            else:
+                exclamation_mark_counter += len(word) + 1
 
-    # Ваш код ниже, возвращайте уже отредактированный текст!
-    return wrong_article
+            word = word.swapcase()[::-1]
+            pointer = word.find(pattern)
+
+            if pointer != -1:
+                word = word[:pointer] + 'cat' + word[pointer + len(pattern):]
+
+            result_list.insert(0, word)
+        result_list[0] = result_list[0][exclamation_mark_counter:].capitalize()
+        result_raw = ' '.join(result_list) + '.\n'
+        sentence += result_raw
+
+    return sentence
+
+print(get_correct_article())
+print(recover_article())
+assert get_correct_article() == recover_article()
