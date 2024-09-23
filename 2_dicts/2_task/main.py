1ba35f84c994c5f30a4d56efc57259c8146bbb04
UrakadakiRemuru <met-zi@mail.ru>
2024-09-23 21:53:11 +0300

    2_task done

--- 2_dicts/2_task/main.py
+++ 2_dicts/2_task/main.py
@@ -1,3 +1,5 @@
+import re
+
 def top_10_most_common_words(text: str) -> dict[str, int]:
     """Функция возвращает топ 10 слов, встречающихся в тексте.
 
@@ -7,5 +9,17 @@ def top_10_most_common_words(text: str) -> dict[str, int]:
     Returns:
         словарь типа {слово: количество вхождений}
     """
+    pattern = r'[.,:;!?—()«»\'"…`  \n]'
     most_common = {}
+    words_list = [elem.lower() for elem in re.split(pattern, text) if len(elem) >= 3]
+
+    for word in words_list:
+        if word in most_common:
+            most_common[word] += 1
+        else:
+            most_common[word] = 1
+
+    l = sorted(list(most_common.items()), key=lambda x: (-x[1], x[0]), reverse=True)[:-11:-1]
+    most_common = dict(l)
+
     return most_common
