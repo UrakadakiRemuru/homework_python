56cef17b0695263cb1da2d15f1f0e90e8642f7fa
UrakadakiRemuru <met-zi@mail.ru>
2024-09-27 22:05:07 +0300

    3_task done

--- 2_dicts/3_task/main.py
+++ 2_dicts/3_task/main.py
@@ -1,4 +1,7 @@
-def format_phone(phone_number: str) -> dict[str, int]:
+import re
+
+
+def format_phone(phone_number: str) -> str:
     """Функция возвращает отформатированный телефон.
 
     Args:
@@ -7,6 +10,16 @@ def format_phone(phone_number: str) -> dict[str, int]:
     Returns:
         отформатированный телефон
     """
-    formatted_phone_number = ""
 
-    return formatted_phone_number
+    pattern = r'[0-9]'
+    formatted_phone_number = ''.join(re.findall(pattern, phone_number))
+    number_length = len(formatted_phone_number)
+    if number_length == 11:
+        if formatted_phone_number[0] == '8':
+            return f'{formatted_phone_number[0]} ({formatted_phone_number[1:4]}) {formatted_phone_number[4:7]}-{formatted_phone_number[7:9]}-{formatted_phone_number[9:]}'
+        elif formatted_phone_number[0] == '7':
+            return f'8 ({formatted_phone_number[1:4]}) {formatted_phone_number[4:7]}-{formatted_phone_number[7:9]}-{formatted_phone_number[9:]}'
+    elif number_length == 10:
+        return f'8 ({formatted_phone_number[0:3]}) {formatted_phone_number[3:6]}-{formatted_phone_number[6:8]}-{formatted_phone_number[8:]}'
+    else:
+        return formatted_phone_number
