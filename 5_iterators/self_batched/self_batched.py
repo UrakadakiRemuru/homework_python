cad1d2abca0f61bb63eb2de0b3823758beddb18c
UrakadakiRemuru <met-zi@mail.ru>
2024-10-21 20:17:16 +0300

    batched done

--- 5_iterators/self_batched/self_batched.py
+++ 5_iterators/self_batched/self_batched.py
@@ -5,8 +5,45 @@ T = TypeVar("T")
 
 def batched(obj: Iterable[T], n: int) -> Generator[tuple[T], None, None]:
     """Пиши свой код здесь."""
+    if n < 1:
+        raise ValueError('n must be at least one')
+    start = 0
+    end = n
+    while True:
+        try:
+            result = tuple(obj[start:end])
+        except IndexError:
+            if start > len(obj):
+                break
+            elif end > len(obj):
+                yield tuple(obj[start:])
+        else:
+            start = end
+            end += n
+            yield result
 
 
 class Batched:
     def __init__(self, obj: Iterable[T], n: int):
-        """Реализуй этот класс."""
+        if n < 1:
+            raise ValueError('n must be at least one')
+        self.obj = list(obj)
+        self.start = 0
+        self.stop = n
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            result = tuple(self.obj[self.start:self.stop])
+        except IndexError:
+            if self.start > len(self.obj):
+                raise StopIteration
+            elif self.stop > len(self.obj):
+                return tuple(self.obj[self.start:])
+        else:
+            self.start = self.stop
+            self.stop += self.n
+            return result
